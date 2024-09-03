0f9fcd5336c6ba4a520999aef238bf0f4f7c9163
sirjana <email@.com>
2024-09-03 18:35:02 +0545

    First commit

---
+++ images/comment.py
@@ -0,0 +1,16 @@
+#yesma chahi jpt lekna paiyo..ani whatever you want to write here, you can write 
+#ijtknbklrjbml
+#ijbklng
+#kjtnbolkrgjb l
+
+
+#comment-(#)
+
+# ijhgktrlopreifkper
+# guyiryf98ioeruf
+# jfihiuerjfoierl
+# iurtgfioerjfdklme
+# uivhireufjocelk
+# igu9triofke;lambdajhggireojfopew'rhgviorelkj
+
+# ufhiekjl
